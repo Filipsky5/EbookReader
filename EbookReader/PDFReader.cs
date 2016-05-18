1f1b503680816477f9465d53ed93c04c983be4fe
Filip Jabłoński <filip.g.jablonski@gmail.com>
2016-05-18 15:19:51 +0200

    add tests and pdf reader

---
+++ EbookReader/PDFReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbookReader
+{
+    public class PDFReader
+    {
+        public PDFReader()
+        {
+
+        }
+        public string read()
+        {
+            return new PDFBook().read();
+        }
+    }
+
+    public class PDFBook
+    {
+        public string read()
+        {
+            return "reading a pdf book";
+        }
+    }
+}
---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EbookReader;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestItCanReadAPDF()
+        {
+            EbookReader.PDFReader r = new EbookReader.PDFReader();
+            StringAssert.Contains(r.read(), "pdf book");
+        }
+    }
+}
